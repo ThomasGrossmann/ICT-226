ee6c41d5574fd5dfc22ded36db03538d1b980d58
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-08-28 10:41:58 +0200

    wip

--- CalculatorTests/ClassLibrary2/ClassLibrary2/Class1.cs
+++ CalculatorTests/ClassLibrary2/ClassLibrary2/Class1.cs
@@ -2,11 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ClassLibrary2
+namespace ClassLibrary1
 {
-    public class Class1
+    public class Calculator
     {
+        #region constructors
+        public Calculator()
+        {
+        }
+        #endregion constructors
+
+        #region public methods
+        public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        public int Substract(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        public int Multiply(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        public int Divide(int operand1, int operand2)
+        {
+            return operand1 / operand2;
+        }
+        #endregion public methods
     }
 }
--- CalculatorTests/ClassLibrary2/UnitTestProject1/UnitTest1.cs
+++ CalculatorTests/ClassLibrary2/UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,87 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using ClassLibrary1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject1
+namespace Test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddNominalCaseSuccess()
         {
+            //given
+            int actualResult;
+            int expectedResult = 6;
+            int op1 = 2;
+            int op2 = 4;
+            Calculator calculator = new Calculator();
+
+            //when
+            actualResult = calculator.Add(op1, op2);
+
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SubstractNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 3;
+            int op1 = 5;
+            int op2 = 2;
+            Calculator calculator = new Calculator();
+
+
+            //when
+            actualResult = calculator.Substract(op1, op2);
+
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void MultiplyNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 15;
+            int op1 = 5;
+            int op2 = 3;
+            Calculator calculator = new Calculator();
+
+
+            //when
+            actualResult = calculator.Multiply(op1, op2);
+
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void DivideNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 4;
+            int op1 = 25;
+            int op2 = 5;
+            Calculator calculator = new Calculator();
+
+
+            //when
+            actualResult = calculator.Divide(op1, op2);
+
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
