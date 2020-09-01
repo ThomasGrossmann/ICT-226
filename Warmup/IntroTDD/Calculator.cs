951b3313ef122cd0e7795ab821770074c3ff835d
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-09-01 10:18:06 +0200

    Start Warmup

---
+++ Warmup/IntroTDD/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroTDD
+{
+    public class Calculator
+    {
+        #region constructors
+        public Calculator()
+        {
+        }
+        #endregion constructorsregion
+
+        #region public methods
+        public int Add(int operande01, int operande02)
+        {
+            return operande01 + operande02;
+        }
+
+        public int Sub(int operande01, int operande02)
+        {
+            return operande01 - operande02;
+        }
+
+        public int Mul(int operande01, int operande02)
+        {
+            return operande01 * operande02
+        }
+        #endregion public methods
+    }
+}
---
+++ Warmup/IntroTDD/TestIntroTDD/TestCalculator.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroTDD;
+
+namespace TestIntroTDD
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        [TestMethod]
+        public void AddNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 6;
+            int op1 = 4;
+            int op2 = 2;
+            Calculator calculator = new Calculator();
+
+            //when
+            actualResult = calculator.Add(op1, op2);
+
+            //then
+            Assert.AreEqual(expectedResult,actualResult);
+        }
+
+        [TestMethod]
+        public void SubNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 2;
+            int op1 = 4;
+            int op2 = 2;
+            Calculator calculator = new Calculator();
+
+            //when
+            actualResult = calculator.Sub(op1, op2);
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void MulNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 8;
+            int op1 = 4;
+            int op2 = 2;
+            Calculator calculator = new Calculator();
+
+            //when
+            actualResult = calculator.Mul(op1, op2);
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void DivNominalCaseSuccess()
+        {
+            //given
+            int actualResult;
+            int expectedResult = 2;
+            int op1 = 4;
+            int op2 = 2;
+            Calculator calculator = new Calculator();
+
+            //when
+            actualResult = calculator.Div(op1, op2);
+
+            //then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
