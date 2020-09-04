64da9c4d85cb8edac5d1d59eccbf0e7fca3dc2c9
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-09-04 14:44:35 +0200

    Create

--- ExcpetionsSurcharge/introtdd/IntroTDD/Calculator.cs
+++ ExcpetionsSurcharge/introtdd/IntroTDD/Calculator.cs
@@ -19,32 +19,6 @@ namespace IntroTDD
         {
             return operande01 + operande02;
         }
-
-        public int Add(int operande01, int operande02, int operande03)
-        {
-            return operande01 + operande02 + operande03;
-            //TODO using a list of integer - NGY ;)
-        }
-
-        public int Sub(int operande01, int operande02)
-        {
-            return operande01 - operande02;
-        }
-
-        public int Mul(int operande01, int operande02)
-        {
-            return operande01 * operande02;
-        }
-
-        /// <summary>
-        /// Doc : https://docs.microsoft.com/en-us/dotnet/api/system.math.round?view=netcore-3.1#Round1_Example
-        /// </summary>
-        public float Div(int operande01, int operande02, bool round = false)
-        {
-            float op1 = (float)operande01;
-            float op2 = (float)operande02;
-            return op1 / op2;
-        }
         #endregion public methods
     }
 }
